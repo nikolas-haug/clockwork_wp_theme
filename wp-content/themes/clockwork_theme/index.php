9eed21c3e1b0e82277995481594be696ac8f80ab
Nikolas Haug <nikohaug21@gmail.com>
2021-05-14 13:48:26 -0500

    update hero cta styles

--- wp-content/themes/clockwork_theme/index.php
+++ wp-content/themes/clockwork_theme/index.php
@@ -1,7 +1,7 @@
 <?php get_header();?>
 
 <!-- posts heading -->
-<section class="container-lg">
+<div class="container-lg">
     <h2 class="posts-heading">
         <?php
 
@@ -25,45 +25,47 @@ if (is_category()) {
     echo $posts_page_title;
 }
 ?>
+</div>
+<!-- Main content -->
+<div class="container-lg">
+    <div class="row">
+        <div class="col-med-8">
+            <?php if (have_posts()): ?>
+            <?php while (have_posts()): the_post();?>
+            <div class="container-stretch padding-right-zero padding-left-zero">
+                <div class="row">
+                    <div class="col-med-8">
+                        </h2>
+                        <h2>
+                            <a href="<?php echo the_permalink(); ?>"><?php the_title();?></a>
+                        </h2>
+                        <div class="meta">
+                            <em><?php the_time('F j, Y g:i a');?></em>
+                            &middot; by
+                            <a
+                                href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author();?></a>
+                        </div>
+                        <?php the_excerpt();?>
+                    </div>
+                    <div class="col-med-4 center-align">
+                        <?php if (has_post_thumbnail()): ?>
+                        <img src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt="<?php the_title();?>"
+                            class="img-100">
+                        <?php endif;?>
+                    </div>
+                </div>
+            </div>
+            <?php endwhile;?>
+            <?php endif;?>
         </div>
-        <!-- Main content -->
-        <section class="container-lg">
-            <div class="row">
-                <div class="col-med-8">
-                    <?php if (have_posts()): ?>
-                    <?php while (have_posts()): the_post();?>
-                    <div class="container-stretch padding-right-zero padding-left-zero">
-                        <div class="row">
-                            <div class="col-med-8">
-    </h2>
-    <h2>
-        <a href="<?php echo the_permalink(); ?>"><?php the_title();?></a>
-    </h2>
-    <div class="meta">
-        <em><?php the_time('F j, Y g:i a');?></em>
-        &middot; by
-        <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author();?></a>
+        <div class="col-med-4">
+            <?php if (is_active_sidebar('sidebar')): ?>
+            <?php dynamic_sidebar('sidebar');?>
+            <?php endif;?>
+        </div>
     </div>
-    <?php the_excerpt();?>
-    </div>
-    <div class="col-med-4 center-align">
-        <?php if (has_post_thumbnail()): ?>
-        <img src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt="<?php the_title();?>" class="img-100">
-        <?php endif;?>
-    </div>
-    </div>
-    </div>
-    <?php endwhile;?>
-    <?php endif;?>
-    </div>
-    <div class="col-med-4">
-        <?php if (is_active_sidebar('sidebar')): ?>
-        <?php dynamic_sidebar('sidebar');?>
-        <?php endif;?>
-    </div>
-    </div>
-    </div>
+</div>
 
 
 
-    <?php get_footer();?>
+<?php get_footer();?>
