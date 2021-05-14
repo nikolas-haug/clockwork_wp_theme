989f9b541f61841ee9af9faf790fd1012a9770be
Nikolas Haug <nikohaug21@gmail.com>
2021-05-14 10:29:50 -0500

    add visually hidden class

--- wp-content/themes/clockwork_theme/header.php
+++ wp-content/themes/clockwork_theme/header.php
@@ -43,6 +43,7 @@ $custom_logo_id = get_theme_mod('custom_logo');
 $logo = wp_get_attachment_image_src($custom_logo_id, 'full');
 if (has_custom_logo()) {
     echo '<img src="' . esc_url($logo[0]) . '" alt="' . get_bloginfo('name') . '" class="img-100 custom-logo">';
+    echo '<h1 class="visually-hidden">' . get_bloginfo('name') . '</h1>';
 } else {
     echo '<h1>' . get_bloginfo('name') . '</h1>';
 }
