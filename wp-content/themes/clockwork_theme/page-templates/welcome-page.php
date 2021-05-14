ab87553b52acdd583abe769d0a2433bd6b6d047b
Nikolas Haug <nikohaug21@gmail.com>
2021-05-14 01:12:48 -0500

    add color palette

--- wp-content/themes/clockwork_theme/page-templates/welcome-page.php
+++ wp-content/themes/clockwork_theme/page-templates/welcome-page.php
@@ -19,7 +19,6 @@ Template Name: Welcome Page
                 <img src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt="<?php the_title();?>"
                     class="img-100">
                 <?php endif;?>
-                <!-- TODO: add button data to customizer -->
                 <?php if (get_theme_mod('welcome_button_route', '')): ?>
                 <?php $action_route = get_theme_mod('welcome_button_route', '');?>
                 <a href="<?php echo esc_url(site_url('/' . $action_route)); ?>" class="button btn--booking btn--fixed">
@@ -36,11 +35,6 @@ Template Name: Welcome Page
         <div class="col-med-12">
             <?php the_content();?>
         </div>
-        <!-- <div class="col-med-4">
-                <?php if (is_active_sidebar('sidebar')): ?>
-                <?php dynamic_sidebar('sidebar');?>
-                <?php endif;?>
-            </div> -->
     </div>
 </div>
 
